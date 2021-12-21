2be99b2f2c4293a801570979381b895949806c30
Samuele Miotto <miotto.samuele@gmail.com>
2021-12-21 14:29:23 +0100

    add header base layout

--- index.php
+++ index.php
@@ -4,10 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
     <link rel="stylesheet" href="style.css">
     <title>php-google-faq</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+
+    <header>
+        <div>
+            <img src="img/google_logo.png" alt="">
+            Privacy e Termini
+        </div>
+        <div>
+            <i class="fas fa-th"></i>
+            <i class="far fa-user"></i>
+        </div>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="">Introduzione</a></li>
+            <li><a href="">Norme sulla Privacy</a></li>
+            <li><a href="">Termini di Servizio</a></li>
+            <li><a href="">Tecnologie</a></li>
+            <li><a href="">Domande Frequenti</a></li>
+        </ul>
+    </nav>
+
+    <main>
+
+    </main>
+
 </body>
 </html>
